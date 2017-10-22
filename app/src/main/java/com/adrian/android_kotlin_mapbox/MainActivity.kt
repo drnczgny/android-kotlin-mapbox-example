30c2a8e8b309ebfb21fc6153379d2186fad6b86a
Adrian Czigany <c.adrian89@gmail.com>
2017-10-22 13:32:58 +0200

    init commit

---
+++ app/src/main/java/com/adrian/android_kotlin_mapbox/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.adrian.android_kotlin_mapbox
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
